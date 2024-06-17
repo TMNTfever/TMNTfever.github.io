ca67589427d71d5b7853022c3b7e7b59ba0e3c9b
Julian-Chris Reyes <julianchris.reyes@gmail.com>
2024-06-17 14:35:12 -0400

    Update transpose.js

--- src/transpose/transpose.js
+++ src/transpose/transpose.js
@@ -12,52 +12,38 @@ Date         Programmer    Change
 2024-06-17   JC Reyes      Written
 ============================================================================
 */
-var chordMap = undefined; // Maps abstract IDs to chord for specific key
+var abMap = undefined; // Maps abstract IDs to chord for specific key
 
-const majSharp = [
+const majTable = [
   ["|1", ">1", "|2", ">2", "|3", "|4", ">4", "|5", ">5", "|6", ">6", "|7"],
-  ["C" , "C#", "D" , "D#", "E" , "F" , "F#", "G" , "G#", "A" , "A#", "B" ],
-  ["G" , "G#", "A" , "A#", "B" , "C" , "C#", "D" , "D#", "E" , "E#", "F#"],
-  ["D" , "D#", "E" , "E#", "F#", "G" , "G#", "A" , "A#", "B" , "B#", "C#"],
   ["A" , "A#", "B" , "B#", "C#", "D" , "D#", "E" , "E#", "F#", "G" , "G#"],
+  ["Bb", "Cb", "C" , "Db", "D" , "Eb", "Fb", "F" , "Gb", "G" , "Ab", "A" ],
+  ["B" , "B#", "C#", "D" , "D#", "E" , "E#", "F#", "G" , "G#", "A" , "A#"], 
+  ["C" , "C#", "D" , "D#", "E" , "F" , "F#", "G" , "G#", "A" , "A#", "B" ],
+  ["C#", "D" , "D#", "E" , "E#", "F#", "G" , "G#", "A" , "A#", "B" , "B#"],
+  ["D" , "D#", "E" , "E#", "F#", "G" , "G#", "A" , "A#", "B" , "B#", "C#"],
+  ["Eb", "Fb", "F" , "Gb", "G" , "Ab", "A" , "Bb", "Cb", "C" , "Db", "D" ],
   ["E" , "E#", "F#", "G" , "G#", "A" , "A#", "B" , "B#", "C#", "D" , "D#"],
-  ["B" , "B#", "C#", "D" , "D#", "E" , "E#", "F#", "G" , "G#", "A" , "A#"],
+  ["F" , "Gb", "G" , "Ab", "A" , "Bb", "Cb", "C" , "Db", "D" , "Eb", "E" ],
   ["F#", "G" , "G#", "A" , "A#", "B" , "B#", "C#", "D" , "D#", "E" , "E#"],
-  ["C#", "D" , "D#", "E" , "E#", "F#", "G" , "G#", "A" , "A#", "B" , "B#"]
+  ["G" , "G#", "A" , "A#", "B" , "C" , "C#", "D" , "D#", "E" , "E#", "F#"],
+  ["Ab", "A" , "Bb", "Cb", "C" , "Db", "D" , "Eb", "Fb", "F" , "Gb", "G" ]
 ];
 
-const minSharp = [
+const minTable = [
   ["|1", ">1", "|2", "|3", ">3", "|4", ">4", "|5", "|6", ">6", "|7", ">7"],
   ["A" , "A#", "B" , "C" , "C#", "D" , "D#", "E" , "F" , "F#", "G" , "G#"],
-  ["E" , "E#", "F#", "G" , "G#", "A" , "A#", "B" , "C" , "C#", "D" , "D#"],
-  ["B" , "B#", "C#", "D" , "D#", "E" , "E#", "F#", "G" , "G#", "A" , "A#"],
-  ["F#", "G" , "G#", "A" , "A#", "B" , "B#", "C#", "D" , "D#", "E" , "E#"],
-  ["C#", "D" , "D#", "E" , "E#", "F#", "G" , "G#", "A" , "A#", "B" , "B#"],
-  ["G#", "A" , "A#", "B" , "B#", "C#", "D" , "D#", "E" , "E#", "F#", "G" ],
-  ["D#", "E" , "E#", "F#", "G" , "G#", "A" , "A#", "B" , "B#", "C#", "D" ],
-  ["A#", "B" , "B#", "C#", "D" , "D#", "E" , "E#", "F#", "G" , "G#", "A" ]
-];
-
-const majFlat = [
-  ["|1", "<2", "|2", "<3", "|3", "|4", "<5", "|5", "<6", "|6", "<7", "|7"],
-  ["F" , "Gb", "G" , "Ab", "A" , "Bb", "Cb", "C" , "Db", "D" , "Eb", "E" ],
-  ["Bb", "Cb", "C" , "Db", "D" , "Eb", "Fb", "F" , "Gb", "G" , "Ab", "A" ],
-  ["Eb", "Fb", "F" , "Gb", "G" , "Ab", "A" , "Bb", "Cb", "C" , "Db", "D" ],
-  ["Ab", "A" , "Bb", "Cb", "C" , "Db", "D" , "Eb", "Fb", "F" , "Gb", "G" ],
-  ["Db", "D" , "Eb", "E" , "F" , "Gb", "G" , "Ab", "A" , "Bb", "B" , "C" ],
-  ["Gb", "G" , "Ab", "A" , "Bb", "B" , "C" , "Db", "D" , "Eb", "E" , "F" ],
-  ["Cb", "C" , "Db", "D" , "Eb", "E" , "F" , "Gb", "G" , "Ab", "A" , "Bb"]
-];
-
-const minFlat = [
-  ["|1", "<2", "|2", "|3", "<4", "|4", "<5", "|5", "|6", "<7", "|7", "<1"],
-  ["D" , "Eb", "E" , "F" , "Gb", "G" , "Ab", "A" , "Bb", "B" , "C" , "Db"],
-  ["G" , "Ab", "A" , "Bb", "B" , "C" , "Db", "D" , "Eb", "E" , "F" , "Gb"],
-  ["C" , "Db", "D" , "Eb", "E" , "F" , "Gb", "G" , "Ab", "A" , "Bb", "B" ],
-  ["F" , "Gb", "G" , "Ab", "A" , "Bb", "B" , "C" , "Db", "D" , "Eb", "E" ],
   ["Bb", "B" , "C" , "Db", "D" , "Eb", "E" , "F" , "Gb", "G" , "Ab", "A" ],
+  ["B" , "B#", "C#", "D" , "D#", "E" , "E#", "F#", "G" , "G#", "A" , "A#"],
+  ["C" , "Db", "D" , "Eb", "E" , "F" , "Gb", "G" , "Ab", "A" , "Bb", "B" ],
+  ["C#", "D" , "D#", "E" , "E#", "F#", "G" , "G#", "A" , "A#", "B" , "B#"],
+  ["D" , "Eb", "E" , "F" , "Gb", "G" , "Ab", "A" , "Bb", "B" , "C" , "Db"],
   ["Eb", "E" , "F" , "Gb", "G" , "Ab", "A" , "Bb", "B" , "C" , "Db", "D" ],
-  ["Ab", "A" , "Bb", "B" , "C" , "Db", "D" , "Eb", "E" , "F" , "Gb", "G" ]
+  ["E" , "E#", "F#", "G" , "G#", "A" , "A#", "B" , "C" , "C#", "D" , "D#"],
+  ["F" , "Gb", "G" , "Ab", "A" , "Bb", "B" , "C" , "Db", "D" , "Eb", "E" ],
+  ["F#", "G" , "G#", "A" , "A#", "B" , "B#", "C#", "D" , "D#", "E" , "E#"],
+  ["G" , "Ab", "A" , "Bb", "B" , "C" , "Db", "D" , "Eb", "E" , "F" , "Gb"],
+  ["G#", "A" , "A#", "B" , "B#", "C#", "D" , "D#", "E" , "E#", "F#", "G" ]
 ];
 
 /*
@@ -65,7 +51,7 @@ const minFlat = [
 Program:   transpose()
 Desc:      Main function to transpose chords.
 Called by: main/chords.html
-Calls:     abstractChords()
+Calls:     N/A
 Arguments: isUp - Boolean value that says whether user wants to transpose
                   up or down.
 Comments:  N/A
@@ -76,63 +62,72 @@ Date         Programmer    Change
 ============================================================================
 */
 function transpose(isUp) {
-  abstractChords();
-  
-  // Transpose, account for table index out-of-bounds, and write to collection
-  // Rebuild chord lines, including whitespace
-  // Rebuild entire chord file and replace id="ukulele-chords"
-
-  if(isUP) {
-  
-  } else {
-  }
-}
-
-/*
-============================================================================
-Program:   abstractChords()
-Desc:      Turns a list of chords into a generalized form so they can be
-           transposed.
-Called by: transpose()
-Calls:     ***AbstractSingle, PopulateKey, CalcIsEntire, CalcChordNum,
-           CalcDifficulty***
-Arguments: N/A
-Comments:  N/A
----------------------------------------------------------------------------
-Date         Programmer    Change
-2019-12-19   JC Reyes      Written in .vba
-2024-06-17   JC Reyes      Converted into .js
-============================================================================
-*/
-function abstractChords() {
-  var chord;
-  var abChord;
-
   // Determine the song's key, and whether it's major or minor
   var songKey = document.getElementById("ukulele-chords").getElementsByTagName("k-")[0].innerHTML;
   //var songKey = songKeyCollection[0].innerHTML;
   var isMaj;
-console.log("key: " + songKey + ", majFlat: " + majFlat[1][0]);
+
   if(songKey.includes("m")) {
     isMaj = false;
     songKey = songKey.substring(0, str.length - 1);
   } else {
-    isMIn = true;
+    isMaj = true;
   }
   
-  // Determine the correct table to use and assign to new variable for manipulation
+  // Assign keyTable to the major or minor table
   var keyTable;
   
-  switch(songKey) {
-    case "":
-    case "":
-    case "":
-      break;
+  if(isMaj) {
+    keyTable = majTable;
+  } else {
+    keyTable = minTable;
   }
   
+  // Determine which row the current key is on in keyTable
+  var keyRow = 0;
+  
+  for(x = 1; x < 13; x++) {
+    if(keyTable[x][0] = songKey) {
+      keyRow = x;
+    }
+  }
+  
+  // Key was not valid, could be a sharp/flat key that is unrepresented
+  if(keyRow = 0) {
+    console.log("Invalid Key: " + songKey);
+    return;
+  }
+  
+  for(x = 0; x < 12; x++) {
+    abMap.set(keyTable[0][x], keyTable[keyRow][x]);
+  }
+console.log(keyTable);
   // Retrieve HTMLCollection of <c-> tags
   var allChords = document.getElementById("ukulele-chords").getElementsByTagName("c-");
+  
+  // Tokenize collections with 3 columns, 1=abstract (|#,<#,>#), 2=flavor, 3=preceding whitespace
 
-  // Tokenize collections with 3 columns, 1-abstract (|#,<#,>#), 2=flavor, 3=preceding whitespace
-  // Populate collection with correct chords
+  // Change abMap depending on transposing direction
+  if(isUP) {
+    if(keyRow = 13) {
+      keyRow = 1;
+    } else {
+      keyRow = keyRow + 1;
+    }
+  } else {
+    (keyRow = 1) {
+      keyRow = 13;
+    } else {
+      keyRow = keyRow - 1;
+    }
+  }
+  
+  for(x = 0; x < 12; x++) {
+    abMap.set(keyTable[0][x], keyTable[keyRow][x]);
+  }
+  
+  // Populate collection with correct chords  
+  // Transpose, account for table index out-of-bounds, and write to collection
+  // Rebuild chord lines, including whitespace
+  // Rebuild entire chord file and replace id="ukulele-chords"
 }
