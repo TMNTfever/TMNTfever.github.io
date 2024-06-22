d58ad2fe79e93431af96a58ec5d0efa7aabba2c8
Julian-Chris Reyes <julianchris.reyes@gmail.com>
2024-06-22 14:45:44 -0400

    transpose-bug-fixes

--- src/transpose/transpose.js
+++ src/transpose/transpose.js
@@ -1,60 +1,63 @@
 /*
-============================================================================
+===============================================================================
 File:      transpose.js
 Desc:      Transposes a song's chords up or down in a Chords page.
 Author:    JC Reyes
 Called by: main/chords.html
 Created:   2024-06-21
 Modified:  2024-06-22
-============================================================================
+===============================================================================
 */
 var chordMap = undefined; // Maps the original-key to the new-key
 var chordMap2 = undefined; // Used for songs with key changes 
 
 const majTable = [
-  ["|1", ">1", "|2", ">2", "|3", "|4", ">4", "|5", ">5", "|6", ">6", "|7"],
-  ["A" , "A#", "B" , "C" , "C#", "D" , "D#", "E" , "F" , "F#", "G" , "G#"],
-  ["Bb", "B" , "C" , "Db", "D" , "Eb", "E" , "F" , "Gb", "G" , "Ab", "A" ],
-  ["B" , "C" , "C#", "D" , "D#", "E" , "F" , "F#", "G" , "G#", "A" , "A#"], 
-  ["C" , "C#", "D" , "D#", "E" , "F" , "F#", "G" , "G#", "A" , "A#", "B" ],
-  ["C#", "D" , "D#", "E" , "F" , "F#", "G" , "G#", "A" , "A#", "B" , "C" ],
-  ["D" , "D#", "E" , "F" , "F#", "G" , "G#", "A" , "A#", "B" , "C" , "C#"],
-  ["Eb", "E" , "F" , "Gb", "G" , "Ab", "A" , "Bb", "B" , "C" , "Db", "D" ],
-  ["E" , "F" , "F#", "G" , "G#", "A" , "A#", "B" , "C" , "C#", "D" , "D#"],
-  ["F" , "Gb", "G" , "Ab", "A" , "Bb", "B" , "C" , "Db", "D" , "Eb", "E" ],
-  ["F#", "G" , "G#", "A" , "A#", "B" , "C" , "C#", "D" , "D#", "E" , "F" ],
-  ["G" , "G#", "A" , "A#", "B" , "C" , "C#", "D" , "D#", "E" , "F" , "F#"],
-  ["Ab", "A" , "Bb", "B" , "C" , "Db", "D" , "Eb", "E" , "F" , "Gb", "G" ]
+  ["|1", ">1", "|2", ">2", "|3", "|4", ">4", "|5", ">5", "|6", ">6", "|7", "X", "Riff"],
+  ["A" , "A#", "B" , "C" , "C#", "D" , "D#", "E" , "F" , "F#", "G" , "G#", "X", "R"],
+  ["Bb", "B" , "C" , "Db", "D" , "Eb", "E" , "F" , "Gb", "G" , "Ab", "A" , "X", "R"],
+  ["B" , "C" , "C#", "D" , "D#", "E" , "F" , "F#", "G" , "G#", "A" , "A#", "X", "R"], 
+  ["C" , "C#", "D" , "D#", "E" , "F" , "F#", "G" , "G#", "A" , "A#", "B" , "X", "R"],
+  ["C#", "D" , "D#", "E" , "F" , "F#", "G" , "G#", "A" , "A#", "B" , "C" , "X", "R"],
+  ["D" , "D#", "E" , "F" , "F#", "G" , "G#", "A" , "A#", "B" , "C" , "C#", "X", "R"],
+  ["Eb", "E" , "F" , "Gb", "G" , "Ab", "A" , "Bb", "B" , "C" , "Db", "D" , "X", "R"],
+  ["E" , "F" , "F#", "G" , "G#", "A" , "A#", "B" , "C" , "C#", "D" , "D#", "X", "R"],
+  ["F" , "Gb", "G" , "Ab", "A" , "Bb", "B" , "C" , "Db", "D" , "Eb", "E" , "X", "R"],
+  ["F#", "G" , "G#", "A" , "A#", "B" , "C" , "C#", "D" , "D#", "E" , "F" , "X", "R"],
+  ["G" , "G#", "A" , "A#", "B" , "C" , "C#", "D" , "D#", "E" , "F" , "F#", "X", "R"],
+  ["Ab", "A" , "Bb", "B" , "C" , "Db", "D" , "Eb", "E" , "F" , "Gb", "G" , "X", "R"]
 ];
 const minTable = [
-  ["|1", ">1", "|2", "|3", ">3", "|4", ">4", "|5", "|6", ">6", "|7", ">7"],
-  ["A" , "A#", "B" , "C" , "C#", "D" , "D#", "E" , "F" , "F#", "G" , "G#"],
-  ["Bb", "B" , "C" , "Db", "D" , "Eb", "E" , "F" , "Gb", "G" , "Ab", "A" ],
-  ["B" , "C" , "C#", "D" , "D#", "E" , "F" , "F#", "G" , "G#", "A" , "A#"],
-  ["C" , "Db", "D" , "Eb", "E" , "F" , "Gb", "G" , "Ab", "A" , "Bb", "B" ],
-  ["C#", "D" , "D#", "E" , "F" , "F#", "G" , "G#", "A" , "A#", "B" , "C" ],
-  ["D" , "Eb", "E" , "F" , "Gb", "G" , "Ab", "A" , "Bb", "B" , "C" , "Db"],
-  ["Eb", "E" , "F" , "Gb", "G" , "Ab", "A" , "Bb", "B" , "C" , "Db", "D" ],
-  ["E" , "F" , "F#", "G" , "G#", "A" , "A#", "B" , "C" , "C#", "D" , "D#"],
-  ["F" , "Gb", "G" , "Ab", "A" , "Bb", "B" , "C" , "Db", "D" , "Eb", "E" ],
-  ["F#", "G" , "G#", "A" , "A#", "B" , "C" , "C#", "D" , "D#", "E" , "F" ],
-  ["G" , "Ab", "A" , "Bb", "B" , "C" , "Db", "D" , "Eb", "E" , "F" , "Gb"],
-  ["G#", "A" , "A#", "B" , "C" , "C#", "D" , "D#", "E" , "F" , "F#", "G" ]
+  ["|1", ">1", "|2", "|3", ">3", "|4", ">4", "|5", "|6", ">6", "|7", ">7", "X", "Riff"],
+  ["A" , "A#", "B" , "C" , "C#", "D" , "D#", "E" , "F" , "F#", "G" , "G#", "X", "R"],
+  ["Bb", "B" , "C" , "Db", "D" , "Eb", "E" , "F" , "Gb", "G" , "Ab", "A" , "X", "R"],
+  ["B" , "C" , "C#", "D" , "D#", "E" , "F" , "F#", "G" , "G#", "A" , "A#", "X", "R"],
+  ["C" , "Db", "D" , "Eb", "E" , "F" , "Gb", "G" , "Ab", "A" , "Bb", "B" , "X", "R"],
+  ["C#", "D" , "D#", "E" , "F" , "F#", "G" , "G#", "A" , "A#", "B" , "C" , "X", "R"],
+  ["D" , "Eb", "E" , "F" , "Gb", "G" , "Ab", "A" , "Bb", "B" , "C" , "Db", "X", "R"],
+  ["Eb", "E" , "F" , "Gb", "G" , "Ab", "A" , "Bb", "B" , "C" , "Db", "D" , "X", "R"],
+  ["E" , "F" , "F#", "G" , "G#", "A" , "A#", "B" , "C" , "C#", "D" , "D#", "X", "R"],
+  ["F" , "Gb", "G" , "Ab", "A" , "Bb", "B" , "C" , "Db", "D" , "Eb", "E" , "X", "R"],
+  ["F#", "G" , "G#", "A" , "A#", "B" , "C" , "C#", "D" , "D#", "E" , "F" , "X", "R"],
+  ["G" , "Ab", "A" , "Bb", "B" , "C" , "Db", "D" , "Eb", "E" , "F" , "Gb", "X", "R"],
+  ["G#", "A" , "A#", "B" , "C" , "C#", "D" , "D#", "E" , "F" , "F#", "G" , "X", "R"]
 ];
 /*
-============================================================================
+===============================================================================
 Program:   transpose()
 Desc:      Main function to transpose chords.
 Called by: main/chords.html
 Calls:     N/A
-Arguments: isUp - Boolean value that says whether user wants to transpose
-                  up or down.
+Arguments: isUp - Boolean value that says whether user wants to transpose the
+                  song up or down.
 Comments:  N/A
----------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 Date         Programmer    Change
 2024-06-21   JC Reyes      Initial version
-2024-06-22   JC Reyes      Added functionality for songs with key changes.
-============================================================================
+2024-06-22   JC Reyes      Added functionality for songs with key changes,
+                           songs that uses riffs on the chords line, and songs
+                           songs have multiple (X) endings. Also added the
+                           cleanLine() function.
+===============================================================================
 */
 function transpose(isUp) {
   // Determine the song's key, and whether it's major or minor
@@ -194,7 +197,7 @@ function transpose(isUp) {
   // Iterate through each line of chords
   for(x = 0; x < chordLines.length; x++) {
     tLine = chordLines[x].innerHTML;
-    tLine = tLine.replace("&gt;", ">"); // Get rid of HTML placeholder for '>'
+    tLine = cleanLine(tLine);
     allTokens.push("<c->");
     var y = 0;
 
@@ -353,7 +356,7 @@ function transpose(isUp) {
 }
 
 /*
-============================================================================
+===============================================================================
 Program:   isSpecial()
 Desc:      Determines if a character is a space, hyphen, pipe, greater-than,
            parenthesis, or a number.
@@ -361,16 +364,40 @@ Called by: transpose()
 Calls:     N/A
 Arguments: char - the character that is being checked
 Comments:  N/A
----------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 Date         Programmer    Change
 2024-06-20   JC reyes      Initial version
-============================================================================
+===============================================================================
 */
-function isSpecial(char) {
-  switch(char){
-    case " ": case "-": case "|": case ">": case "(": case ")":
+function isSpecial(chara) {
+  switch(chara){
+    case " ": case "-": case "|": case ">": case "(": case ")": case ":":
       return true;
     default:
       return false;
   }
+}
+
+/*
+===============================================================================
+Program:   cleanLine()
+Desc:      Looks for uncommon note names, like E# or Cb, and replaces them with
+           the more common names, F and B, respectively. Also looks for HTML
+           placehodler IDs for special characters and replaces them too.
+Called by: transpose()
+Calls:     N/A
+Arguments: line - String which is a line of chords
+Comments:  N/A
+-------------------------------------------------------------------------------
+Date         Programmer    Change
+2024-06-22   JC reyes      Initial version
+===============================================================================
+*/
+function cleanLine(line) {
+  line = line.replaceAll("&gt;", ">"); // Get rid of HTML placeholder for '>'
+  line = line.replaceAll("E#", "F");
+  line = line.replaceAll("Fb", "E");
+  line = line.replaceAll("B#", "C");
+  line = line.replaceAll("Cb", "B");
+  return line;
 }
