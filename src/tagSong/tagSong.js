4af38b88facac6c91a5511fd66dd18a623c8b718
Julian-Chris Reyes <julianchris.reyes@gmail.com>
2025-06-21 17:41:34 -0400

    more-text-songs

--- src/tagSong/tagSong.js
+++ src/tagSong/tagSong.js
@@ -5,7 +5,7 @@ Desc:      Applies desired tags for css styling. Converts .txt to .html.
 Author:    JC Reyes
 Called by: main/chords.html
 Created:   2025-06-10
-Modified:  2025-06-16
+Modified:  2025-06-21
 ===============================================================================
 */
 
@@ -19,8 +19,11 @@ Arguments: fileName - The song file to be manipulated.
 Comments:  N/A
 -------------------------------------------------------------------------------
 Date        Programmer  Change
-2024-06-10  JC Reyes    Initial version.
-2024-06-16  JC Reyes    Added ability to add <detail> tags.
+2025-06-10  JC Reyes    Initial version.
+2025-06-16  JC Reyes    Added ability to add <detail> tags.
+2025-06-21  JC Reyes    Fixed logic on determining the end of a section. Used
+                        to wait for another occurence of '[', but now looks for
+                        an empty line.
 ===============================================================================
 */
 function tagSong() {
@@ -72,24 +75,15 @@ function tagSong() {
     temp = lines[i] || "";
     char0 = temp.charAt(0);
     
+    // The line is not an empty string
     if (lines[i] !== "") {
       // Section found
       if (char0 === '[') {
         chordLine = true;
         char5 = temp.charAt(5);
         
-        // Details tag is open
-        if (detOpen){
-          lines[i - 1] += "</details>";
-          detOpen = false;
-        }
-        
-        // Div for "focus" is open
-        if (divOpen){
-          lines[i - 1] += "</div>";
-          divOpen = false;
-        }
-        
+        // Assign open and closing tags depending on section name
+        // Goes off the 5th index due to being unique identifiers
         switch(char5) {
           case 'O': case 'E': case 'C': // [intrO] [outrO] [versE] or [pre-Chorus]
             openTag = "<s->";
@@ -104,8 +98,10 @@ function tagSong() {
             openTag = "<b->";
             closeTag = "</b->";
             break;
-          default:
+          default: // Unrecognized section name, give default colors
             console.log("Invalid section name.");
+            openTag = "<s->";
+            closeTag = "</s->";
         }
         
         // Section has a description within parenthesis, or a repeat
@@ -120,7 +116,7 @@ function tagSong() {
           else if (temp.includes("Same") && closeTag !== "</r->"){
             lines[i] = "<details><summary>" + openTag + temp.slice(0, endBrackIndex) + closeTag + temp.slice(endBrackIndex) + "</summary>";
             detOpen = true;
-          }
+          } // Regular section heading
           else {
             lines[i] = openTag + temp.slice(0, endBrackIndex) + closeTag + temp.slice(endBrackIndex);
           }
@@ -132,7 +128,7 @@ function tagSong() {
       else if (char0 === '-') {
         lines[i] = "<s->" + temp + "</s->";
       } // EOF reached
-      else if (i == lines.length - 1 && divOpen) {
+      else if ((i == lines.length - 1) && divOpen) {
         lines[i - 1] += "</div>";
         divOpen = false;
       } // Chord or Lyric line found (just alternates)
@@ -145,6 +141,19 @@ function tagSong() {
           lines[i] = "<l->" + temp + "</l->";
           chordLine = true;
         }
+      }
+    } // Empty line reached, which is the end of a section
+    else {
+      // Details tag is open
+      if (detOpen){
+        lines[i] += "</details>";
+        detOpen = false;
+      }
+      
+      // Div for "focus" is open
+      if (divOpen){
+        lines[i] += "</div>";
+        divOpen = false;
       }
     }
   }
